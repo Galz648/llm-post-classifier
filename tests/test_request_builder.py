abcd4b79026bb1cea654a3993bc6e658f9de112a
Gal Zafar <theycallmefachfach@gmail.com>
2025-01-31 22:08:27 +0200

    test: Refactor RequestBuilder test suite
    * Updated test case for text support to verify message content

--- tests/test_request_builder.py
+++ tests/test_request_builder.py
@@ -3,22 +3,18 @@ from auto_post_classifier.request_builder import RequestBuilder
 import pytest
 
 
-def test_request_throws_if_config_is_not_provided():
+def test_request_throws_if_config_is_not_provided_on_build():
     builder = RequestBuilder()
 
     with pytest.raises(ValueError):
         builder.build()
 
 
-def test_request_builder_throws_if_text_is_not_provided():
-    builder = RequestBuilder()
-
-    with pytest.raises(ValueError):
-        builder.add_text_support("")
-
-
 def test_request_builder_adds_text_support():
     builder = RequestBuilder()
     builder.add_text_support("test")
 
-    assert builder.config.messages[0].content[0].text == "test"
+    assert builder.config.messages.pop().content.pop().text == "test"
+    assert len(builder.config.messages) == 1 # the system prompt message is added by default; TODO: determine if this is coupled to the implementation
+
+
