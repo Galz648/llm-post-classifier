eefaf8eb1ea9314f2e5c6f248d588d3269cd45cb
Gal Zafar <theycallmefachfach@gmail.com>
2025-02-04 18:15:56 +0200

    refactor: Update Post model with optional fields and image support

--- auto_post_classifier/models.py
+++ auto_post_classifier/models.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
+# TODO: change names to image_url, text, content_url, platform
 class Post(BaseModel):
-    text: str
+    image: Optional[str] = None
+    text: Optional[str] = None
     content_url: str
